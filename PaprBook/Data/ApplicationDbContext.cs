38edd165b0b222872eab5c05511009a034c2cfaa
Luvyansh <luvyansh2001@gmail.com>
2025-04-30 03:00:22 +0530

    Add project files.

---
+++ PaprBook/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PaprBook.Models;
+
+namespace PaprBook.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Document> Documents { get; set; }
+    }
+}
---
+++ PaprBook/Models/Document.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PaprBook.Models
+{
+    public class Document
+    {
+        public int id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        [Required]
+        public string? UserId { get; set; }
+        [ForeignKey("UserId")]
+        public Microsoft.AspNetCore.Identity.IdentityUser? User { get; set; }
+    }
+}
